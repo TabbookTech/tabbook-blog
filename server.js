9e47a70efc2adda413c1da2484faba4b1b90c0a6
Tri Mai Minh <mingti@Tris-MacBook-Pro.local>
2021-11-12 11:29:46 +0700

    commit changes for add post

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const config = require('./config')
 const adminRouters = require('./routes/adminRouter')
 
 const app = express()
-
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 app.use('/admin', adminRouters.routers)
