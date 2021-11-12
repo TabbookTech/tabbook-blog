6048f7a79df9f7d58902cf014414dcd310a60725
TabbookTech <tabbooktechteam@gmail.com>
2021-11-12 08:52:14 +0700

    second commit with getposts/update/delete api added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,4 @@
 const firebase = require('firebase')
-const { firebaseConfig } = require('../config')
 const db = require('../db')
 const firestore = firebase.firestore()
 
@@ -8,13 +7,57 @@ const addBlog = async (req, res, next) => {
     try {
         const data = req.body
         await firestore.collection('posts').doc().set(data)
-        res.send('Record added successfully')
+        res.send('New Post added successfully')
     } catch (err) {
         res.status(400).send(err.message)
     }
 }
 
+const getAllPosts = async (req, res, next) => {
+    try{
+        const posts = await firestore.collection('posts');
+        const data = await posts.get();
+        const postArray = []
+        if (data.empty) {
+            res.status(400).send('No post is found');
+        } else {
+            data.forEach(doc => {
+                let ele = doc.data()
+                ele['id'] = doc.id
+                postArray.push(ele);
+            })
+            res.send(postArray)
+        }
+    }catch(err){
+        res.status(400).send(err.message)
+    }
+}
+
+const updateOnePost = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const data  = req.body;
+        const post = await firestore.collection('posts').doc(id)
+        await post.update(data)
+        res.send('Post updated successfully');
+    } catch (err) {
+        res.status(400).send('Update failed')
+    }
+}
+
+const deleteOnePost = async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        await firestore.collection('posts').doc(id).delete()
+        res.send('Post deleted successfully');
+    } catch (err) {
+        res.status(400).send('Delete failed')
+    }
+}
 
 module.exports = {
     addBlog,
+    getAllPosts,
+    updateOnePost,
+    deleteOnePost
 }
--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const { addBlog } = require('../controllers/adminController')
+const { addBlog, getAllPosts, updateOnePost, deleteOnePost } = require('../controllers/adminController')
 
 const router = express.Router();
 
 router.post('/add-post', addBlog);
+router.get('/all-posts', getAllPosts);
+router.put('/update-post/:id', updateOnePost);
+router.delete('/delete-post/:id', deleteOnePost);
 
 module.exports = {
     routers: router
